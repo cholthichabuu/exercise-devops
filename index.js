c1f41b7c972d4c9b9c7ccc300f203b8822275459
Cholthicha Nakhot <60160206@go.buu.ac.th>
2020-11-07 18:16:25 +0700

    add unit-test

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const item = [
+    {id: 1, name:'iphone 12 Pro Max'},
+    {id: 2, name:'Google Pixel 5'}
+]
 
 app.get('/', (req, res) => {
     return res.send('Hello!')
 })
 
+app.get('/item', (req, res) => {
+    return res.send(item)
+})
+
+
 app.listen(port, () => {
-    console.log(`Start Express application at ${port}`)
+    console.log('Start Express application at ${port}')
 })
 
 module.exports = app
